e9cb2cff36fa1f8941b72c323110c9ed70f5de8c
hunter Mac <hunter10@naver.com>
2017-07-26 17:03:12 +0900

    Sound 처리

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,13 +8,55 @@ public class UIManager : MonoBehaviour {
     public static UIManager Instance;
     public Text scoreText;
 
+    public int[] crystalNumForChan;
+    public Dropdown CrystalDropdown;
+
     void Awake()
     {
         Instance = this;
     }
 
+    private void Start()
+    {
+        SetDropDown();
+    }
+
     public void UpdateScore(int score)
     {
         scoreText.text = "Score : " + score;
+    }
+
+    public void SoundToggle(Toggle tog)
+    {
+        if(tog.isOn)
+        {
+            if(tog.name == "SoundOn")
+            {
+                Debug.Log("Sound on");
+            }
+            else 
+            {
+                Debug.Log("Sound off");
+            }
+        }
+    }
+
+    void SetDropDown()
+    {
+        CrystalDropdown.options.Clear();
+        for (int i = 0; i < crystalNumForChan.Length; i++)
+        {
+            Dropdown.OptionData data = new Dropdown.OptionData{
+                text = crystalNumForChan[i].ToString()
+            };
+            CrystalDropdown.options.Add(data);
+
+        }
+        CrystalDropdown.captionText.text = crystalNumForChan[0].ToString();
+    }
+
+    public void DropDownChanged(Dropdown drop)
+    {
+        Debug.Log("Selected value is " + crystalNumForChan[drop.value]);
     }
 }
